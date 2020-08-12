66b997a7158288cbfb32a35785601f7dd71e624b
John Moons <jmoons@realdigitalmedia.com>
2020-08-12 14:41:50 -0400

    Abstracting column headers and value delimiter

--- combined_log_parser.rb
+++ combined_log_parser.rb
@@ -2,7 +2,7 @@ class CombinedLogParser
 
   GREP_STRING                       = "done: "
   GREPPED_OUTPUT_REGULAR_EXPRESSION = /([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]*Z) [a-zA-Z0-9\s\S]+ ([a-zA-Z::]+::[a-zA-Z]+) JID-([a-zA-Z0-9]+)[a-zA-Z0-9\s\S]+ ([0-9]*.[0-9]+) sec/
-  CSV_COLUMN_HEADER_LABELS          = "DateTime,Job Name,Job ID,Job Duration,blank,Unique Job,Min (s),Max (s),Average (s),Job Count"
+  DELIMITER                         = "\t"
 
   def initialize( arguments )
     @arguments = arguments
@@ -20,12 +20,26 @@ class CombinedLogParser
     # Split the grepped output into an array of lines
     grep_output = grep_output.split("\n")
 
-    puts CSV_COLUMN_HEADER_LABELS
+    puts column_header_labels.map{ | label | label << DELIMITER }.join
     grep_output.each do | grep_line |
       match_data = GREPPED_OUTPUT_REGULAR_EXPRESSION.match( grep_line )
-      puts "#{match_data[1]},#{match_data[2]},#{match_data[3]},#{match_data[4]}"
+      puts "#{match_data[1]}#{DELIMITER}#{match_data[2]}#{DELIMITER}#{match_data[3]}#{DELIMITER}#{match_data[4]}"
     end
 
+  end
+
+  def column_header_labels
+    [
+      "DateTime",
+      "Job Name",
+      "Job ID",
+      "Duration",
+      "Unique Job",
+      "Min (s)",
+      "Max (s)",
+      "Average (s)",
+      "Job Count"
+    ]
   end
 
   def output_spreadsheet_formula
