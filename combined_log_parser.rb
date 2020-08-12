07513d453e118a9a8778180d9d496baed9323072
John Moons <jmoons@realdigitalmedia.com>
2020-08-12 14:35:29 -0400

    Changing over to a Class structure and laying the ground work for having the program perform two duties

--- combined_log_parser.rb
+++ combined_log_parser.rb
@@ -1,18 +1,36 @@
-GREP_STRING                       = "done: "
-GREPPED_OUTPUT_REGULAR_EXPRESSION = /([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]*Z) [a-zA-Z0-9\s\S]+ ([a-zA-Z::]+::[a-zA-Z]+) JID-([a-zA-Z0-9]+)[a-zA-Z0-9\s\S]+ ([0-9]*.[0-9]+) sec/
-CSV_COLUMN_HEADER_LABELS          = "DateTime,Job Name,Job ID,Job Duration,blank,Unique Job,Min (s),Max (s),Average (s),Job Count"
+class CombinedLogParser
 
-# Grab the raw log file
-input_log_file = ARGV[0]
+  GREP_STRING                       = "done: "
+  GREPPED_OUTPUT_REGULAR_EXPRESSION = /([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]*Z) [a-zA-Z0-9\s\S]+ ([a-zA-Z::]+::[a-zA-Z]+) JID-([a-zA-Z0-9]+)[a-zA-Z0-9\s\S]+ ([0-9]*.[0-9]+) sec/
+  CSV_COLUMN_HEADER_LABELS          = "DateTime,Job Name,Job ID,Job Duration,blank,Unique Job,Min (s),Max (s),Average (s),Job Count"
 
-# Grep the file
-grep_output = `grep -i #{GREP_STRING} #{input_log_file}`
+  def initialize( arguments )
+    @arguments = arguments
 
-# Split the grepped output into an array of lines
-grep_output = grep_output.split("\n")
+    @arguments ? process_combined_log_file : output_spreadsheet_formula
+  end
 
-puts CSV_COLUMN_HEADER_LABELS
-grep_output.each do | grep_line |
-  match_data = GREPPED_OUTPUT_REGULAR_EXPRESSION.match( grep_line )
-  puts "#{match_data[1]},#{match_data[2]},#{match_data[3]},#{match_data[4]}"
+  private
+
+  def process_combined_log_file
+
+    # Grep the file
+    grep_output = `grep -i #{GREP_STRING} #{@arguments}`
+
+    # Split the grepped output into an array of lines
+    grep_output = grep_output.split("\n")
+
+    puts CSV_COLUMN_HEADER_LABELS
+    grep_output.each do | grep_line |
+      match_data = GREPPED_OUTPUT_REGULAR_EXPRESSION.match( grep_line )
+      puts "#{match_data[1]},#{match_data[2]},#{match_data[3]},#{match_data[4]}"
+    end
+
+  end
+
+  def output_spreadsheet_formula
+    puts "OSF"
+  end
 end
+
+CombinedLogParser.new( ARGV[0] )
